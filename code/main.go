87f897d3e6542fdfec8697b871d3addceb7dc670
shahchhatru <shahkhesavchhatra@gmail.com>
2024-03-30 21:33:43 +0545

    util folder added

--- code/main.go
+++ code/main.go
@@ -2,11 +2,16 @@ package main
 
 import "github.com/gofiber/fiber/v2"
 
+import (
+    "myapp/code/utils"
+    
+)
+
 func main() {
     app := fiber.New()
 
     app.Get("/", func(c *fiber.Ctx) error {
-        return c.SendString("Chhatra Bikram is  back")
+        return c.SendString(utils.TestUtilMessage())
     })
 
 	app.Listen(":8080")
---
+++ code/utils/test_util.go
@@ -0,0 +1,7 @@
+// Package utils provides utility functions
+package utils
+
+// TestUtilMessage returns a test message
+func TestUtilMessage() string {
+    return "Hello from test util folder"
+}
