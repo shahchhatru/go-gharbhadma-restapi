eab12b27fa6c586f02f71bb621a5b9ec3dbe7613
shahchhatru <shahkhesavchhatra@gmail.com>
2024-03-31 11:56:25 +0545

    mysql and go server in same docker compose

--- code/main.go
+++ code/main.go
@@ -1,18 +1,34 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
-
 import (
-    "myapp/code/utils"
-    
+    "gorm.io/gorm"
+    "gorm.io/driver/mysql"
+    "log"
 )
 
 func main() {
-    app := fiber.New()
+    // MySQL connection string
+    dsn := "root:123@tcp(dev-db:3306)/nest?charset=utf8mb4&parseTime=True&loc=Local"
 
-    app.Get("/", func(c *fiber.Ctx) error {
-        return c.SendString(utils.TestUtilMessage())
-    })
+    // Open a connection to the database
+    
+    
+    db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+    if err != nil {
+        log.Fatalf("Error connecting to database: %v", err)
+    }
+    
 
-	app.Listen(":8080")
+    // Ping the database to check connectivity
+    sqlDB, err := db.DB()
+    if err != nil {
+        log.Fatalf("Error getting underlying SQL database: %v", err)
+    }
+    defer sqlDB.Close()
+
+    // Database connection successful
+    log.Println("Connected to MySQL database")
+
+    // You can now use the 'db' object to interact with the database
+    // For example, you can define your models and perform CRUD operations
 }
